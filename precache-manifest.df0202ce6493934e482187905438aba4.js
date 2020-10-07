ea7a328b44a175ac86eeffb2a62008763ef9be30
Kaeneth Dayao <kendayao@gmail.com>
2020-10-07 10:59:20 -0700

    Updates

---
+++ precache-manifest.df0202ce6493934e482187905438aba4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdb16016ae728b3c1dcc5953f205845e",
+    "url": "/To-Do-List/index.html"
+  },
+  {
+    "revision": "e1e6e437247df5be45cc",
+    "url": "/To-Do-List/static/css/main.e0aa7a2a.chunk.css"
+  },
+  {
+    "revision": "3f76bfd968581874d475",
+    "url": "/To-Do-List/static/js/2.ff977aae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-List/static/js/2.ff977aae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1e6e437247df5be45cc",
+    "url": "/To-Do-List/static/js/main.aa80d8c2.chunk.js"
+  },
+  {
+    "revision": "3fc2ca48924706e73961",
+    "url": "/To-Do-List/static/js/runtime-main.ce724f51.js"
+  }
+]);
